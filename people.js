869a9509c68ad4a29387f2d77a1344d011863a26
ruku <devnull@localhost>
2014-11-27 15:08:58 -0500

    voici voici les petite personne

---
+++ people.js
@@ -0,0 +1,12 @@
+function people(id)
+{
+
+
+  var lefthand = document.getElementById(id).getElementsByName('lefthand')[0];
+  var righthand = document.getElemementById(id).getElementByName('righthand')[0];
+
+  this.movearm = function() {
+    var vitesse = 0.1;
+
+  }
+}
